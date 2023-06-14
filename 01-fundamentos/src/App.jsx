bf0b431db08aa8c7210ac8495b7d47711900ca5c
Cezar Molinar <cezarmolinar@gmail.com>
2023-06-14 08:07:59 -0300

    estrutura minima

---
+++ 01-fundamentos/src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Post 
+        author="Nome do autor"
+        content="Loren ipsum dolor sit amet consectur Loren ipsum dolor sit amet consectur"
+      />
+    </div>
+  )
+}
