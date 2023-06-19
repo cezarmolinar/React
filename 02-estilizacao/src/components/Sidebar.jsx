dc47148859914814d844a68e35541bd71d115b42
Cezar Molinar <cezarmolinar@gmail.com>
2023-06-19 12:02:17 -0300

    responsividade

--- 02-estilizacao/src/components/Sidebar.jsx
+++ 02-estilizacao/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react'
 
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar(){
   return (
@@ -10,10 +11,7 @@ export function Sidebar(){
         src= "https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGRldmVsb3BlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=300&q=50" 
       />
       <div className={styles.profile}>
-        <img 
-          className={styles.avatar} 
-          src= "https://avatars.githubusercontent.com/u/12806963?v=4" 
-        />                
+        <Avatar src="https://avatars.githubusercontent.com/u/12806963?v=4" />
         <strong>Cezar A. Molinar</strong>
         <span>Web Developer</span>
       </div>
