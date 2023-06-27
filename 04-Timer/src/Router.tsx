1c6363f092e16ad113304a5f60486b72c0a49d1c
Cezar Molinar <cezarmolinar@gmail.com>
2023-06-27 12:31:23 -0300

    Layout Home page finalizada

--- 04-Timer/src/Router.tsx
+++ 04-Timer/src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import { History } from './pages/History'
 
-import { Home } from './pages/Home'
+import { Home } from './pages/Home/Home'
 import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
