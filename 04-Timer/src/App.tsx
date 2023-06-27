10e08cb7dff264babe9a0d4b64c293f4fb287df9
Cezar Molinar <cezarmolinar@gmail.com>
2023-06-27 09:10:12 -0300

    Definição do header

--- 04-Timer/src/App.tsx
+++ 04-Timer/src/App.tsx
@@ -1,15 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button color="primary" />
-      <Button color="secondary" />
-      <Button color="success" />
-      <Button color="danger" />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
--- 04-Timer/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVarColor } from './Button.styles'
-
-interface IButtonProps {
-  color?: ButtonVarColor
-}
-
-export function Button({ color = 'primary' }: IButtonProps) {
-  return <ButtonContainer varColor={color}>Enviar</ButtonContainer>
-}
