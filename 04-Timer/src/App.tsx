9b0157a25e3dea23ac13c530f74a5184d0728808
Cezar Molinar <cezarmolinar@gmail.com>
2023-06-30 09:19:42 -0300

    exibição dos históricos

--- 04-Timer/src/App.tsx
+++ 04-Timer/src/App.tsx
@@ -3,14 +3,15 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
-// import { ComponentePai } from './pages/home2'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
-  // return <ComponentePai />
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
