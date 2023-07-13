f97ceb286bd9c3d295da380811ea28743513b467
Cezar Molinar <cezarmolinar@gmail.com>
2023-07-13 09:15:10 -0300

    busca por filtro

--- 05-Consumo-API/src/pages/Transactions/components/SearchForm/index.tsx
+++ 05-Consumo-API/src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,41 @@
 import { MagnifyingGlass } from 'phosphor-react'
 import { SearchFormContainer } from './styles'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+
+const serachFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof serachFormSchema>
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque pro transações" />
+  const { fetchTransactions } = useContext(TransactionsContext)
 
-      <button type="submit">
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(serachFormSchema),
+  })
+
+  async function handleSubmitTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSubmitTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque pro transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
